81278c0fecccafbb101a6e541d2839ee6623a2bc
Yu Tang <herrtangvonjj@gmail.com>
2025-08-24 21:26:16 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from importlib import import_module
-
-flow = import_module('.flow',  __name__)
-cell = import_module('.cell',  __name__)
-node = import_module('.node',  __name__)
-net  = import_module('.net',   __name__)
-
-__all__ = ['flow', 'cell', 'node', 'net'] 
